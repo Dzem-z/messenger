b9ea215b7fd718765fe873fdfd725bc230525624
Dzem-z <egneradam@gmail.com>
2025-03-20 19:53:03 +0100

    Added titles for chat types and added empty list for private chats.

--- frontend/src/Components/ChatBar.js
+++ frontend/src/Components/ChatBar.js
@@ -9,21 +9,28 @@ function ChatBar({ chats, userName }) {
     
     
 
-    return <ul>
-        {chats.map((chat, index) =>
-            <li key={index}>
+    return <div>
+            <div className="text-block navChat-block">Private chats</div>
+            <ul>
+            
+            </ul>
+            <div className="text-block navChat-block">Public chats</div>
+            <ul>
                 
+            {chats.filter((chat) => !chat.isPrivate).map((chat, index) =>
+                <li key={index}>
                     <NavLink to={"/chat/" + chat.idToken} onClick={() => setSelectedChat(index)}>
                         <div className={`tile chat ${selectedChat == index ? "selected" : ""}`}>
                             {chat.name}
                         </div>
                     </NavLink>
                 
-            </li>)}
-            </ul>;
+                </li>)}
+            </ul>
+        </div>;
 };
 
-var chats = [
+var chatsV = [
     {id:1, name:"chat1"},
     {id:2, name:"chat2"}
 ];
