aff6f6ae5aeb030b8e51eb28aa0d524fbb63789a
Dzem-z <egneradam@gmail.com>
2025-03-22 11:47:30 +0100

    Added Private chat list in ChatBar.

--- frontend/src/Components/ChatBar.js
+++ frontend/src/Components/ChatBar.js
@@ -1,6 +1,7 @@
 
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import processChatName from "../process/ProcessChatName";
 
 function ChatBar({ chats, userName }) {
     const [selectedChat, setSelectedChat] = useState(null);
@@ -12,7 +13,14 @@ function ChatBar({ chats, userName }) {
     return <div>
             <div className="text-block navChat-block">Private chats</div>
             <ul>
-            
+            {chats.filter((chat) => chat.isPrivate).map((chat, index) =>
+                <li key={index}>
+                    <NavLink to={"/chat/" + chat.idToken} onClick={() => setSelectedChat(index)}>
+                        <div className={`tole chat ${selectedChat == index ? "selected" : ""}`}>
+                            {processChatName(chat.name)}
+                        </div>
+                    </NavLink>
+                </li>)}
             </ul>
             <div className="text-block navChat-block">Public chats</div>
             <ul>
